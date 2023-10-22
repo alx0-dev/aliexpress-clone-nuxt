b969386c975a3fd95c83db4c96c0bd559a0b731b
alx0 <57196291+alx0-dev@users.noreply.github.com>
2023-10-22 16:09:05 +0800

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,83 @@
+<template>
+    <MainLayout>
+        <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+            <div
+                class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4"
+            >
+                <div v-if="products" v-for="product in products" :key="product">
+                    <ProductComponent :product="product" />
+                </div>
+            </div>
+        </div>
+    </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from "../layouts/MainLayout.vue";
+
+const products = [
+    {
+        id: 1,
+        title: "Title 1",
+        description: "Description 1",
+        url: "https://picsum.photos/id/7/800/800",
+        price: 9999,
+    },
+    {
+        id: 2,
+        title: "Title 2",
+        description: "Description 2",
+        url: "https://picsum.photos/id/7/800/800",
+        price: 4342,
+    },
+    {
+        id: 3,
+        title: "Title 3",
+        description: "Description 3",
+        url: "https://picsum.photos/id/7/800/800",
+        price: 3333,
+    },
+    {
+        id: 4,
+        title: "Title 4",
+        description: "Description 4",
+        url: "https://picsum.photos/id/7/800/800",
+        price: 6456,
+    },
+    {
+        id: 5,
+        title: "Title 5",
+        description: "Description 5",
+        url: "https://picsum.photos/id/7/800/800",
+        price: 1234,
+    },
+    {
+        id: 6,
+        title: "Title 6",
+        description: "Description 6",
+        url: "https://picsum.photos/id/7/800/800",
+        price: 65456,
+    },
+    {
+        id: 7,
+        title: "Title 7",
+        description: "Description 7",
+        url: "https://picsum.photos/id/7/800/800",
+        price: 324,
+    },
+    {
+        id: 8,
+        title: "Title 8",
+        description: "Description 8",
+        url: "https://picsum.photos/id/7/800/800",
+        price: 2345,
+    },
+    {
+        id: 9,
+        title: "Title 9",
+        description: "Description 9",
+        url: "https://picsum.photos/id/7/800/800",
+        price: 6554,
+    },
+];
+</script>
